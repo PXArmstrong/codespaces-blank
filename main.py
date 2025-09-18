732dc3b5b26192ed63fe3831e886d48011f19fa5
Kurfyoo <143193879+Kurfyoo@users.noreply.github.com>
2025-09-18 22:30:14 +0000

    added spaces

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-i=1
+i = 1
 print("Welcome to the food order service menu")
-name=input("What is your name? ")
-Birthday=int(input("Please enter your birthyear: "))
+name = input("What is your name? ")
+birthday = int(input("Please enter your birthyear: "))
 #Gathers information from the user
-age=2025-Birthday
-#Calculates the Age based off of birthday
-options=["placeholder1","placeholder2","placeholder3","exit"]
+age = 2025 - birthday
+#Calculates the age based off of birthday
+options = ["placeholder1","placeholder2","placeholder3","exit"]
 for word in options:
     print(str(i)+"."+word)
-    i+=1
+    i += 1
 userinp= input("Please enter the number of your choice ")
-if userinp==4:
+if userinp == 4:
     exit()
 #List Printer and Exit selection
