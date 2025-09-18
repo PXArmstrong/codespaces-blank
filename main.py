345fc1e2fd90ae1ac9113503eb0e830a85ed7247
PXArmstrong <pharmstrong04@gmail.com>
2025-09-18 22:03:12 +0000

    Fixed Grammer changers

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-
+i=1
 print("Welcome to the food order service menu")
 name=input("What is your name?")
+Birthday=int(input("Please enter your birthyear"))
+age=2025-Birthday
+options=["placeholder1","placeholder2","placeholder3","exit"]
+for word in options:
+    print(str(i)+"."+word)
+    i+=1
+userinp= input("Please enter the number of your choice")
+if userinp==4:
+    exit()
