724a8bf9cf78c3e6df5ed43ce69a11c11c8de68a
PXArmstrong <pharmstrong04@gmail.com>
2025-09-18 22:21:56 +0000

    Added Comments

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ i=1
 print("Welcome to the food order service menu")
 name=input("What is your name? ")
 Birthday=int(input("Please enter your birthyear: "))
+#Gathers information from the user
 age=2025-Birthday
+#Calculates the Age based off of birthday
 options=["placeholder1","placeholder2","placeholder3","exit"]
 for word in options:
     print(str(i)+"."+word)
@@ -10,3 +12,4 @@ for word in options:
 userinp= input("Please enter the number of your choice ")
 if userinp==4:
     exit()
+#List Printer and Exit selection
