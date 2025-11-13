ddd172f9662a4ae2402176cc13711afb9d8c76d3
PXArmstrong <pharmstrong04@gmail.com>
2025-11-13 23:32:00 +0000

    Final Touches

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ def main():
     commonIssue=["Account Issues","Fund Transfering","Card Issues","Other"]
     options=["Open a new Account","Report an Issue","Contact a Human Representative","Exit"]
     print("--------------------------------------------------- ")
-    print("First National Bank System:")
+    print("First National Bank Customer Services:")
+
     for word in options:
         print(str(i)+"."+word)
         i+=1
     #Lists Out the Users options
+
     userinp= str(input("Please enter the number of your choice "))
     if userinp=="4" or userinp=="exit" :
         exit()
@@ -28,6 +30,8 @@ def main():
             print("Okay! Now there is one last step. Please provide us with your SSN,ID,or proof of adress and one of our employees will step up your account shortly")
             proof=str(input("Please submit proof of identity:"))         
     #If user chooses to make an account,Presents option between checking and Savings and then asks for proof of ID to open account.
+
+
     if userinp=="2" or userinp=="Report Issue":
         print("What is the issue your facing?")
         issue=str(input("Please describe your problem in detail:"))
@@ -35,6 +39,9 @@ def main():
             print("Thank you for reporting this problem, our staff will notify you as soon as fix is solved. Have a good day!")
             quit
     #Allows the user to report issues they are having directly to the banking staff.
+
+
+
     if userinp=="3":
         iq=1
         print("Okay! Before we get you to a human representative, what seems to be the general problem you are having?")
@@ -56,13 +63,19 @@ def main():
             print("We are connecting your to our Customer Service branch to further take a look at your inqueries")
             exit()          
         #Has a list of reasons to talk to a human and moves the user to one of those lines
-num=-10
+
+
+
 print("\033[1m"+"Welcome to the First National Bank Banking System")
+print("-------------------------------------------------------")
 name=input("What is your name? ")
-account_id=str(input("Please enter your Account ID:"))
+account_id=str(input("Please enter your Account ID: "))
 if(len(account_id)>7 or len(account_id)<7):
     print("This is not a viable Account ID, please try again");
     quit
+#Introduction Sequence
+
+num=-10
 while num<0:
     main()
 #Runs the code until the user selects to exit or gets "redirected" somewhere else
